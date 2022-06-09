f573a91bbf5c5e1962f1241b4b5ea386e1f11d42
qnosin <putaj.jakub@gmail.com>
2022-06-09 19:22:52 +0200

    fixis for netifly

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react'
-import Header from '../components/Header'
 import { useNavigate } from 'react-router-dom'
 function ErrorPage() {
     let navigate = useNavigate()
